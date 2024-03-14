51fa0a2d814f5270d915e41a9f3d8dffe5492c81
BenedictBenjamin <siwoo98511@gmail.com>
2024-03-14 11:08:33 +0900

    tailwind t

--- src/types/info.ts
+++ src/types/info.ts
@@ -5,7 +5,6 @@ type Lng = number;
 // 위도와 경도를 묶어준 타입
 export type Coordinates = [Lat, Lng];
 // json 에서 온 데이터를 위한 타입정의
-
 export type Info = {
   coordinates: Coordinates;
   //   건강증진센터구분?: string;
