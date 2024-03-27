a1aa906779abc15c16cf453e60a1db39c0c1dd0a
BenedictBenjamin <siwoo98511@gmail.com>
2024-03-27 10:45:07 +0900

    페키지 제이슨 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://health-center-devgreact.vercel.app',
+  siteUrl: process.env.SITE_URL || 'https://health-center-47fe.vercel.app/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
