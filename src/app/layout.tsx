e570ba5f37116634d521f561f1b787502a90ef08
BenedictBenjamin <siwoo98511@gmail.com>
2024-03-27 10:19:31 +0900

    헬스 등록 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 export const metadata: Metadata = {
@@ -12,6 +13,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="naver-site-verification"
+          content="8d0adb20f574d2b9ce583dffb9646df469aecb0f"
+        />
+        <link
+          rel="canonical"
+          href="https://health-center-devgreact.vercel.app"
+        ></link>
+
+        <meta
+          name="google-site-verification"
+          content="Q2bHOLs6jaFYy4NK_gCCdUCjf270wQ8zYE_nerDd7lw"
+        />
+        <GoogleAnalytics gaId="G-L57MXLTT6K" />
+      </head>
       <body>{children}</body>
     </html>
   );
